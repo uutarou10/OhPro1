cc90c68a6c9f3632f2fb67acc7c9ab369bc2b427
Kota Nonaka <kota.kaicho@gmail.com>
2016-06-30 13:41:27 +0900

    fix Class

--- src/kg11/Kadai03.java
+++ src/kg11/Kadai03.java
@@ -1,14 +1,14 @@
 package kg11;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by Kota Nonaka on 2016/06/30.
  */
 public class Kadai03 {
     public static void main(String[] args) {
-        Map<String,Integer> map = new HashMap<>();
+        Map<String,Integer> map = new TreeMap<>();
         map.put("Humburger",100);
         map.put("Cheeseburger",130);
         map.put("Teriyaki",310);
