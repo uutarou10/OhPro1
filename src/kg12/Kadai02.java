446dae88b82a58ea4fc4382ad964181d2cffbaf2
Kota Nonaka <kota.kaicho@gmail.com>
2016-07-07 13:43:32 +0900

    add Kadai02

---
+++ src/kg12/Kadai02.java
@@ -0,0 +1,14 @@
+package kg12;
+
+/**
+ * Created by Kota Nonaka on 2016/07/07.
+ */
+public class Kadai02 {
+    public static void main(String[] args) {
+        Point[] points  = new Point[5];
+        for (int i = 0; i < points.length; i++){
+            points[i] = new Point();
+            System.out.println(points[i]);
+        }
+    }
+}
